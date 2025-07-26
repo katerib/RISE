996eae31e6ee534f21c98a41a92f12243bc25d96
Abdullah <66182508+zahinabrer5@users.noreply.github.com>
2025-07-26 16:17:17 +0600

    Add mars time

---
+++ assets/js/time.js
@@ -0,0 +1,24 @@
+function fmtDate(date) {
+    return date.toISOString().split('.')[0].replace('T', ' ');
+}
+
+function updateTime() {
+    const timeDisplays = document.getElementsByClassName('time');
+    const doc = document.documentElement;
+    const isMars = doc.getAttribute('data-theme') === 'mars';
+
+    const earthDate = new Date();
+    const marsDate = new MarsDate(earthDate);
+    const earthDateFmt = fmtDate(earthDate);
+    const marsDateFmt = fmtDate(marsDate);
+
+    for (const timeDisplay of timeDisplays) {
+        timeDisplay.innerHTML = `<b>${isMars ? marsDateFmt : earthDateFmt}</b>`;
+        timeDisplay.setAttribute('title', isMars ? marsDate.toDateString() : earthDate.toDateString());
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateTime();
+    setInterval(updateTime, 1000);
+});
